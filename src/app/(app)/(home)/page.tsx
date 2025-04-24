7044c6822d0f48e8089ff495bc68729ef2cada1b
rakibcoding <rhbappy456@gmail.com>
2025-04-24 12:39:53 +0600

    enhance categories collection

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -8,7 +8,10 @@ export default async function Home() {
 
   const data = await payload.find({
     collection: "categories",
+    depth: 1, // 1 = only top level categories, 2 = all categories
+    where: { parent: { exists: false } },
   });
+  console.log(data);
 
   return <div className="">{JSON.stringify(data, null, 2)}</div>;
 }
