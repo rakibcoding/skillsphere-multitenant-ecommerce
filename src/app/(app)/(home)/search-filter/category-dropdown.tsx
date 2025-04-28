dbe5ec4c6c9e730e748d85f5ca0323399fd62a14
rakibcoding <rhbappy456@gmail.com>
2025-04-29 00:03:35 +0600

    fixed category hover issues

--- src/app/(app)/(home)/search-filter/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filter/category-dropdown.tsx
@@ -42,7 +42,8 @@ export const CategoryDropdown = ({
         <Button
           className={cn(
             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-            isActive && !isNavigationHovered && "bg-white border-primary"
+            isActive && !isNavigationHovered && "bg-white border-primary",
+            isOpen && "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]",
           )}
           variant="elevated"
         >
