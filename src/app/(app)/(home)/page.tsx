bf36fe9908483d416a950af63735ccee457d254d
rakibcoding <rhbappy456@gmail.com>
2025-04-30 19:43:52 +0600

    tRPC setup

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useTRPC } from "@/trpc/client";
+// import { getQueryClient, trpc } from "@/trpc/server";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+  const trpc = useTRPC();
+  // const queryClient = getQueryClient();
+
+  //Server side query
+  // const categories = queryClient.fetchQuery(
+  //   trpc.categories.getMany.queryOptions()
+  // );
+  //Client side query
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+
   return (
     <div className="">
-      <h2>Home</h2>
+      <h2>Home{JSON.stringify(categories.data, null, 2)}</h2>
     </div>
   );
 }
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const DMSans = DM_Sans({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${DMSans.className}  antialiased`}>{children}</body>
+      <body className={`${DMSans.className}  antialiased`}>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
     </html>
   );
 }
