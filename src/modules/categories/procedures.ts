4d440f2f89537d7a9aa6365a9d5deb701a079b26
rakibcoding <rhbappy456@gmail.com>
2025-04-30 20:06:29 +0600

    fetch all data

--- src/modules/categories/procedures.ts
+++ src/modules/categories/procedures.ts
@@ -1,7 +1,28 @@
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Category } from "@/payload-types";
 
 export const categoriesRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
-    return [{ hello: "world" }];
+    const payload = await getPayload({
+      config: configPromise,
+    });
+    const data = await payload.find({
+      collection: "categories",
+      pagination: false,
+      depth: 1, // 1 = only top level categories, 2 = all categories
+      where: { parent: { exists: false } },
+      sort: "name",
+    });
+    const formattedData = data.docs.map((doc) => ({
+      ...doc,
+      subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+        ...(doc as Category),
+        subcategories: undefined,
+      })), // bcz of 'depth: 1' we are confident 'doc' will be a type of 'Category'
+    }));
+
+    return formattedData
   }),
 });
