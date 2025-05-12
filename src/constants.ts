1275a01578dc6a5663db53623261d8099a1401f6
rakibcoding <rhbappy456@gmail.com>
2025-05-12 19:49:19 +0600

    14-Products-List-UI

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_LIMIT = 6;
+export const DEFAULT_LIMIT = 8;
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -3,11 +3,14 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Sort, Where } from "payload";
 import { Category, Media } from "@/payload-types";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -83,6 +86,8 @@ export const productsRouter = createTRPCRouter({
         depth: 1, // 1 = populate "category", "images"
         where,
         sort,
+        page: input.cursor,
+        limit: input.limit,
       });
 
       return {
