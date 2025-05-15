1c70c0cd9671d090dd9f83924bca9630d18023b8
rakibcoding <rhbappy456@gmail.com>
2025-05-15 12:35:08 +0600

    21. Library

--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -10,7 +10,6 @@ import { StarRating } from "../star-rating";
 import { Button } from "@/components/ui/button";
 import { LinkIcon, StarIcon } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
-// import { CartButton } from "../components/cart-button";
 
 const CartButton = dynamic(
   () => import("../components/cart-button").then((mod) => mod.CartButton),
@@ -103,7 +102,11 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
             <div className="border-t lg:border-t-0 lg:border-l h-full">
               <div className="flex flex-col gap-4 p-6 border-b">
                 <div className="flex flex-row items-center gap-2">
-                  <CartButton productId={productId} tenantSlug={tenantSlug} />
+                  <CartButton
+                    isPurchased={data.isPurchased}
+                    productId={productId}
+                    tenantSlug={tenantSlug}
+                  />
                   <Button
                     variant="elevated"
                     className="size-12"
