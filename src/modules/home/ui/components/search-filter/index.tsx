dbba96a030b846da6cc75a60503e7598b6c500e1
rakibcoding <rhbappy456@gmail.com>
2025-05-20 20:03:07 +0600

    fixed suspense errors

--- src/modules/home/ui/components/search-filter/index.tsx
+++ src/modules/home/ui/components/search-filter/index.tsx
@@ -7,11 +7,12 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
 import { BreadcrumbNavigation } from "./breadcrumb-navigation";
+import { useProductFilters } from "@/modules/products/hooks/use-product-filter";
 
 export const SearchFilter = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
-
+  const [filters, setFilters] = useProductFilters();
   const params = useParams();
   const categoryParam = params.category as string | undefined;
   const activeCategory = categoryParam || "all";
@@ -30,7 +31,12 @@ export const SearchFilter = () => {
       className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
       style={{ backgroundColor: activeCategoryColor }}
     >
-      <SearchInput />
+      <SearchInput
+        defaultValue={filters.search}
+        onChange={(value) => {
+          setFilters({ search: value });
+        }}
+      />
       <div className="hidden lg:block">
         <Categories data={data} />
       </div>
